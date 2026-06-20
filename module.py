d645dcf8ec97f464dd5661b7f01ac381403056b6
Rohit Koli <33840139+rohkoli94@users.noreply.github.com>
2026-06-20 15:59:27 +0530

    chapter 1 practice completed

---
+++ module.py
@@ -0,0 +1,23 @@
+import pyjokes
+
+
+# single line comment s...")
+
+joke = pyjokes.get_joke()
+print(joke)
+
+joke2 = pyjokes.get_jokes()
+print(joke2)
+
+"""
+multi line comment
+test1
+test2
+"""
+
+
+'''
+multi line comment
+test3
+test4
+'''
---
+++ practise-three.py
@@ -0,0 +1,9 @@
+import os
+
+# Printing current directory
+print("Current directory: ", os.getcwd())
+
+#List contents of directory
+print("List contents of directory: ", os.listdir())
+
+
